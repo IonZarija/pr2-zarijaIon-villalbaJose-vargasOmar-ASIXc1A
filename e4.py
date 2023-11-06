4b14f141653f7b3f7bdca188f6be86464c2b9e75
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-11-06 12:36:35 +0100

    a

--- e4.py
+++ e4.py
@@ -2,5 +2,14 @@
 Ion Zarija, José Villalba, Omar Vargas
 06/11/2023
 ASIXc M03 UF1 A3 PR3
-isBigger.py
+customerLoyaltyCard.py
 """
+baseimp = float(input("Introduce el importe base: "))
+ptotal = 0
+if baseimp >= 100:
+    ptotal = baseimp*0.79
+else:
+    ptotal = baseimp
+
+print("El importe es de: ", ptotal*1.21)
+
