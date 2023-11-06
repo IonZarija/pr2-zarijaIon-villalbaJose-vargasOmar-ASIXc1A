271de5bd9284a4bb013c0fe35574475432be72e7
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-11-06 12:07:37 +0100

    a

--- e2.py
+++ e2.py
@@ -4,4 +4,12 @@ Ion Zarija, José Villalba, Omar Vargas
 ASIXc M03 UF1 A3 PR3
 areGrowingNumbers.py
 """
+num1 = int(input("Numero 1?: "))
+num2 = int(input("Numero 2?: "))
+num3 = int(input("Numero 3?: "))
 
+if num1 <= num2 <= num3 and num1 <= num3:
+    print("Orden creciente")
+
+else:
+    print("No orden creciente")
