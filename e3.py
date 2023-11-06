209c281529ab64c3f310cd5ba26ef54b98a5f180
ion.zarija.7e5 <ion.zarija.7e5@itb.cat>
2023-11-06 12:19:18 +0100

    a

--- e3.py
+++ e3.py
@@ -2,5 +2,17 @@
 Ion Zarija, José Villalba, Omar Vargas
 06/11/2023
 ASIXc M03 UF1 A3 PR3
-isBigger.py
+fastAndFurious.py
 """
+
+velIni = int(input("Velocitat Inicial?: "))
+acc = int(input("Acceleració?: "))
+temps = int(input("Temps?: "))
+
+velIns = velIni + acc * temps
+
+if velIns <= 0:
+    print("Està parat i no es pot calcular la velocitat mitjana")
+else:
+    velMitj = (velIns + velIni)/2
+    print (f"La velocitat mitjana és {velMitj}")
