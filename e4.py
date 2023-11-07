fc6de50eb4713c6f7546885b48451d5982acabe8
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-11-07 07:58:32 +0100

    a

--- e4.py
+++ e4.py
@@ -5,21 +5,18 @@ ASIXc M03 UF1 A3 PR3
 customerLoyaltyCard.py
 """
 
-try:
-    baseimp = float(input("Introduce el importe base: "))
-except:
-    print("STRING FAILURE")
-    tarjeta = str(input("El usuario tiene en su pertenencia tarjeta?: "))
-    ptotal = 0
-    match tarjeta:
-        case "si":
-            if baseimp >= 100:
-                ptotal = baseimp * 0.79
-            else:
-                ptotal = baseimp
-            print("El importe es de: ", ptotal * 1.21)
-        case _:
-            print("El usuario no tiene descuento")
+baseimp = float(input("Introduce el importe base: "))
+tarjeta = str(input("El usuario tiene en su pertenencia tarjeta?: "))
+ptotal = 0
+match tarjeta:
+    case "si":
+        if baseimp >= 100:
+            ptotal = baseimp * 0.79
+        else:
+            ptotal = baseimp
+        print("El importe es de: ", ptotal * 1.21)
+    case _:
+        print("El usuario no tiene descuento")
 
 
 
