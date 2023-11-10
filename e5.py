495275ddc40fd18865ef542f530ff529f83da7af
ion.zarija.7e5 <ion.zarija.7e5@itb.cat>
2023-11-10 11:54:24 +0100

    a

--- e5.py
+++ e5.py
@@ -4,23 +4,36 @@ Ion Zarija, José Villalba, Omar Vargas
 ASIXc M03 UF1 A3 PR3
 isBigger.py
 """
-
-date = input().split('/')
-year = int(date[2])
-month = int(date[1])
-day = int(date[0])
-dateCorrect = (1 <= month <=12 and 1<= day <= 31)
-bisiesto = False
-if dateCorrect:
-    if date[1] == 2:
-        if year%4 == 0 and year%100 != 0 or year%400 == 0:
-            bisiesto = True
-        if day > 29 and bisiesto == True:
-            print("No es vàlido")
-    if day > 28 and bisiesto == False:
-        print("No es valido")
-else:
-    print("No es valid")
+try:
+    date = input().split('/')
+    year = int(date[2])
+    month = int(date[1])
+    day = int(date[0])
+    dateCorrect = (1 <= month <= 12 and 1 <= day <= 31)
+    if dateCorrect:
+        if month == 2:
+            if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+                if day > 29:
+                    print("No es vàlido")
+                else:
+                    print("Es valido")
+            elif day > 28:
+                print("No es valido")
+            else:
+                print("Es valido")
+        else:
+            match month:
+                case 1 | 3 | 5 | 7 | 8 | 10 | 12:
+                    print("Es valido")
+                case 4 | 6 | 9 | 11:
+                    if day > 30:
+                        print("No es valido")
+                    else:
+                        print("Es valido")
+    else:
+        print("No es valid")
+except ValueError:
+    print("Data no valida")
 
 
 
