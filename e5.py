ca3376ddc24a9bcce590ff12fd6e0cf9d4361170
ion.zarija.7e5 <ion.zarija.7e5@itb.cat>
2023-11-10 11:22:32 +0100

    a

--- e5.py
+++ e5.py
@@ -6,8 +6,21 @@ isBigger.py
 """
 
 date = input().split('/')
-
-#bisiesto año%4 =0 AND año%100 != 0 OR año%400 = 0
+year = int(date[2])
+month = int(date[1])
+day = int(date[0])
+dateCorrect = (1 <= month <=12 and 1<= day <= 31)
+bisiesto = False
+if dateCorrect:
+    if date[1] == 2:
+        if year%4 == 0 and year%100 != 0 or year%400 == 0:
+            bisiesto = True
+        if day > 29 and bisiesto == True:
+            print("No es vàlido")
+    if day > 28 and bisiesto == False:
+        print("No es valido")
+else:
+    print("No es valid")
 
 
 
